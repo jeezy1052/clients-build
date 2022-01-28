07ccda059281c63c2b3e6bfbb36a106708ba171f
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-01-27 19:01:26 -0800

    ff

--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Review = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    defaultValues: {
+      firstName: '',
+      strainName: ''
+    }
+  });
+  console.log(errors)
   return (
     <div className=" bg-green-200 w-100 h-screen grid gap-8 grid-cols-2 grid-rows-1 justify-items-center justify-center ">
     <div className="text-center md:max-w-lg p-5">
@@ -11,31 +19,51 @@ const Review = () => {
     </div>
     <div>
     <div class="w-full max-w-xs">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+  onSubmit={handleSubmit((data) => {
+    console.log(data)
+  })}
+  >
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="firstname">
         Username
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+      <input
+
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"
+      {...register("firstname", {required: "This is required"})}
+
+      />
     </div>
+    <p>{errors.firstname?.message}</p>
     <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="strainname">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="strainName">
         Strain Name
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="strainname" type="text" placeholder="Username"/>
+      <input
+
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="strainName" type="text" placeholder="Username"
+      {...register("strainName",{required: "This is required"})}
+      />
+      <p>{errors.strainName?.message}</p>
+     
     </div>
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="help">
         Password
       </label>
-      <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="help" type="text" placeholder="rating" />     
+    
+      <input class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="help" type="text" placeholder="rating"
+      {...register('help', {required: "This is required", minLength: 4, message: "Min length is 4"})}
+       />     
+      <p>{errors.firstName?.message}</p>
     </div>
     
     <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      <button class="bg-green-800 hover:bg-green-200 text-white hover:text-green-800 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
         Sign In
       </button>
-      <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+      <a class="inline-block align-baseline font-bold text-sm text-green-800 hover:text-green-800" href="#">
         Forgot Password?
       </a>
     </div>
